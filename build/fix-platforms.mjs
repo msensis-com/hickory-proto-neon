09328f0bd8ef6ec80c8affe4535fcf7aa26b3b54
Angelos Bouklis <me@arjix.dev>
2024-11-13 11:14:23 +0000

    update fix-platforms.mjs

--- build/fix-platforms.mjs
+++ build/fix-platforms.mjs
@@ -7,10 +7,14 @@ if (pkg.optionalDependencies)
     pkg.optionalDependencies = Object.fromEntries(Array.prototype.map.call(
         Object.entries(pkg.optionalDependencies),
         ([key, value]) => {
-            const platform = key.match(/@msensis-com\/(.+)/)?.[1];
+            let platform = key.match(/@msensis-com\/(.+)/)?.[1];
+            if (!platform) return null;
+            if (platform === "win32-x64-msvc") {
+                platform = "windows-x64-gnu";
+            }
             return [`@msensis/hickory-proto-neon-${platform}`, value];
         }
-    ));
+    ).filter(Boolean));
 }
 
 writeFileSync("package.json", JSON.stringify(pkg, null, 2));
